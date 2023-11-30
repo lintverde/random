c79faad736addc828dd95ff69c19790750cb0f1f
lintverde <152469336+lintverde@users.noreply.github.com>
2023-11-30 03:25:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ RNG.prototype.choiceB = function (len) {
 var rng = new RNG(isNaN(game) ? 10 : parseInt(game));
 
 var firstDigit  = ["0", "1", "2", "3", "4", "5"];
-var secondDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8",];
+var secondDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 for (var i = 0; i < round *2; i++) {
   resultArray[i] = rng.choiceB(6);
   i++;
